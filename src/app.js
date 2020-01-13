635e5e2cf061c13a369ede331fe87af090150dd1
Jen Merritt <jenjomez@gmail.com>
2020-01-13 20:08:44 +0000

    complete styling

--- src/app.js
+++ src/app.js
@@ -4,10 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-
+      toDoList: [
+        {description: "Supermarket shop", priority: "low"},
+        {description: "Call sister", priority: "high"},
+        {description: "Buy birthday gift", priority: "high"}
+      ],
+      newToDo: { description: "", priority: "high"}
     },
     methods: {
-
+      saveNewToDo: function() {
+        const list = this.toDoList;
+        const newToDo = this.newToDo;
+        list.push(newToDo);
+        this.newToDo = {description: "", priority: "high"};
+      }
     }
   });
 });
